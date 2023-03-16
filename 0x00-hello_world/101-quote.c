582f5b62bb0594b5c29b719fc639e655dc30c724
aceinvestor <amadichi99@gmail.com>
2023-03-16 14:53:17 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -  C program that prints
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
+
+
