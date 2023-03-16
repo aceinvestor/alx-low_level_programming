8d3cce97251694de0149e722b83ab4476c6172f7
aceinvestor <amadichi99@gmail.com>
2023-03-16 12:13:37 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main -  C program that prints sizes
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int size1 = 32;
+	int size2 = 64;
+
+	printf("size%d", size1);
+	printf("size%d", size2);
+	return (0);
+}
