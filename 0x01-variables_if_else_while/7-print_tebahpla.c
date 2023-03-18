5be1fbf098c1fddedf5ceda79524fcec34839c20
aceinvestor <amadichi99@gmail.com>
2023-03-17 19:16:24 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+		putchar(!x);
+	}
+		putchar('\n');
+	return (0);
+}
