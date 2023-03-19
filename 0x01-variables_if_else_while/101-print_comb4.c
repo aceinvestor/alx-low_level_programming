b1df22898a95585312525e714c40423584e8463b
aceinvestor <amadichi99@gmail.com>
2023-03-19 01:26:34 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,6 +20,10 @@ int main(void)
 				putchar(x + '0');
 				putchar(y + '0');
 				putchar(z + '0');
+				if (x == 7 && y == 8)
+				{
+					break;
+				}
 				putchar(',');
 				putchar(' ');
 			}
