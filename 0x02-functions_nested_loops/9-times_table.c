c10bd4004d495e43d31d5337b1bdb3044d0e7ff2
aceinvestor <amadichi99@gmail.com>
2023-03-21 12:23:11 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,5 +7,18 @@
  */
 void times_table(void)
 {
+	int x;
+	int y;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		for (y = 0; y <= 9 ; y++)
+		{
+			_putchar(x + '0');
+			_putchar(',');
+			_putchar(y + '0');
+			_putchar(',');
+		}
+	}
 
 }
