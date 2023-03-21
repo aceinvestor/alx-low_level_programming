7cc2dc672208c480a367dd55023c33ff59b910e8
aceinvestor <amadichi99@gmail.com>
2023-03-21 13:18:47 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,10 @@ void print_to_98(int n)
 {
 	for (n = 0 ; n <= 98 ; n++)
 	{
-		if (n == 98)
-		{
-			printf("%d ", n);
-		}
-		else
-		{
-			printf("%d, ", n);
-		}
+		printf("%d, ", n);
+	}
+	for (n = 0 ; n > 98 ; n--)
+	{
+		printf("%d, ", n);
 	}
 }
