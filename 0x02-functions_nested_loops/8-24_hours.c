00a5ef26f0a086958667713d542cd21b66060de5
aceinvestor <amadichi99@gmail.com>
2023-03-21 10:47:59 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,9 @@ void jack_bauer(void)
 	int c;
 	int d;
 
-	for (a = 0 ; a <= 5 ; a++)
+	for (a = 0 ; a <= 2 ; a++)
 	{
-		for (b = 0 ; b <= 9 ; b++)
+		for (b = 0 ; b <= 3 ; b++)
 		{
 			for (c = 0 ; c <= 5 ; c++)
 			{
