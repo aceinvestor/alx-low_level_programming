88ccc79eedfb6f7dc839a7c67c3ee732d4d22d33
aceinvestor <amadichi99@gmail.com>
2023-03-21 11:01:37 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,9 +2,9 @@
 
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer,
- *  starting from 00:00 to 23:59
+ * starting from 00:00 to 23:59
  *
- *  Return: 0
+ *  Return: void
  */
 void jack_bauer(void)
 {
