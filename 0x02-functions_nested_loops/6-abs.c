7a903e5e0bb008034c54d9ab13ca5f38f313e6d2
aceinvestor <amadichi99@gmail.com>
2023-03-21 09:35:29 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,15 +11,15 @@ int _abs(int x)
 {
 	if (x > 0)
 	{
-		_putchar(x);
+		return (x);
 	}
 	else if (x == 0)
 	{
-		_putchar(-x);
+		return (-x);
 	}
 	else
 	{
-		_putchar(x);
+		return (x);
 	}
 	return (0);
 }
