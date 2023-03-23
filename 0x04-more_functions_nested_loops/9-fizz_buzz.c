b3532a7b92e84f7a0905b9f093e1affa1e9f7a04
aceinvestor <amadichi99@gmail.com>
2023-03-23 16:21:13 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 
 	for (x = 1 ; x <= 100 ; x++)
 	{
-		if (x % 3 == 0)
+		if (x % 15 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("fizz");
 		}
 		else if (x % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if (x % 15 == 0)
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
