e640fd7fb5d4ae2367102f939d27420ac127ef9c
aceinvestor <amadichi99@gmail.com>
2023-03-29 13:27:33 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,15 @@
 /**
  * _strcmp - function that compares two strings
  *
+ * @s1: string
+ * @s2: string
+ *
  * Return: 0
  */
 int _strcmp(char *s1, char *s2)
 {
-	return strcmp(s1, s2);
+	int acex;
+
+	acex = strcmp(s1, s2);
+	return (acex);
 }
