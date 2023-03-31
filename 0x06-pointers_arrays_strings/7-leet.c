adac4031d345071396965fa53c823872667e56cb
aceinvestor <amadichi99@gmail.com>
2023-03-30 18:24:19 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *leet -  function that encodes a string into 1337
+ *
+ * @n: string
+ *
+ * Return: 0
+ */
+char *leet(char *n)
+{
+	int i, j;
+
+	char a1[] = "aAeEoOtTlL";
+	char a2[] = "4433007711";
+
+	for (i = 0 ; n[i] != '\0' ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			if (n[i] == a1[j])
+			{
+				n[i] = a2[j];
+			}
+		}
+	}
+	return (n);
+}
