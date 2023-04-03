65fcbd514a03fb6f64afe323514e247f37119da4
aceinvestor <amadichi99@gmail.com>
2023-04-03 09:40:27 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strstr - function that locates a substring
+ *
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *f = strstr(haystack, needle);
+
+	if (f == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (f);
+	}
+}
