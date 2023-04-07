26f6815ad21b8af29584d3ebccc6d49f89280d87
aceinvestor <amadichi99@gmail.com>
2023-04-07 13:46:26 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  program that multiplies two numbers
+ *
+ * @argc: number of argument
+ * @argv: number of array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int multiply = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			multiply *= atoi(argv[i]);
+		}
+		printf("%d\n", multiply);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
