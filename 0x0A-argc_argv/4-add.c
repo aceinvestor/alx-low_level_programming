3f7653cd3f35673ab8e400c139c15c803dd9105b
aceinvestor <amadichi99@gmail.com>
2023-04-08 10:09:02 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,12 +12,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1 ; i < argc ; i++)
 	{
-		int j;
-
 		for (j = 0 ; argv[i][j] != '\0' ; j++)
 		{
 			if (!isdigit(argv[i][j]))
