6abba0d09b185e0bb82259388661160a88fc1971
aceinvestor <amadichi99@gmail.com>
2023-04-13 07:42:13 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = len2;
 	}
-	str = malloc(len1 + n + 1);
+	str = (char *)malloc(len1 + n + 1 * sizeof(char));
 	if (str == NULL)
 	{
 		return (NULL);
