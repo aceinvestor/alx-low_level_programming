34033c64a592335155259cce36a7ff466690fbbb
aceinvestor <amadichi99@gmail.com>
2023-04-13 10:50:33 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,15 +17,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size);
 		return (ptr);
 	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	free(ptr);
+
 	ptr = malloc(new_size);
 	return (ptr);
 }
