12f7948d1f76312134a92402971a833a268e199a
aceinvestor <amadichi99@gmail.com>
2023-04-17 14:09:50 -0700

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#define dog_t struct dog
 
 /**
  * struct dog - new type
@@ -9,12 +8,12 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
