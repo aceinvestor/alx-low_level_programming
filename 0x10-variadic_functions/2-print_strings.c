87514bec4777c1ca62b3f3e264abba64728a9bf4
aceinvestor <amadichi99@gmail.com>
2023-04-20 08:37:14 -0700

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(list, char*);
 		if (string == NULL)
 		{
-			printf("(nil)");
+			string = "(nil)";
 		}
 		if (separator != NULL)
 		{
@@ -41,4 +41,3 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	printf("\n");
 	va_end(list);
 }
-
