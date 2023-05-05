8fe7ebea0ffffc490bee14a11420de69a096fcca
aceinvestor <amadichi99@gmail.com>
2023-05-05 10:31:11 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,20 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-	for (i = 31; i >= 0; i--)
-	{
-		_putchar(((n >> i) & 1) ? '1' : '0');
-	}
+	_putchar((n & 1) + '0');
 }
